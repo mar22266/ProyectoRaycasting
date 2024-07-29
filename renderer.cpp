bd26509e4eabaff84deeed1b9240f0e4c60a6557
Andre Marroquin <mar22266@uvg.edu.gt>
2024-07-29 10:14:21 -0600

    Feat: advances

--- renderer.cpp
+++ renderer.cpp
@@ -1,8 +1,12 @@
 #include "renderer.h"
 #include <SFML/Graphics/PrimitiveType.hpp>
 #include <SFML/Graphics/Vertex.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <cmath>
+#include <cstddef>
+#include <limits>
+#include <vector>
 
 constexpr float PI = 3.141592653589793f;
 constexpr size_t MAX_RAYCAST_DEPTH = 16;
@@ -11,65 +15,88 @@ void Renderer::drawRays(sf::RenderTarget &target, const Player &player, const Ma
 {
    float angle = player.angle * PI / 180.0f;
    float vtan = -std::tan(angle);
-   float htan = 1.0f / std::tan(angle);
+   float htan = -1.0f / std::tan(angle);
    float cellSize = map.getCellSize();
 
-   sf::Vector2f rayPos, offset;
+   size_t vdof = 0, hdof = 0;
+   float vdist = std::numeric_limits<float>::max();
+   float hdist = std::numeric_limits<float>::max();
+
+   sf::Vector2f vRayPos, hRayPos, offset;
    if (std::cos(angle) > 0.001f)
    {
-      rayPos.x = std::floor(player.position.x / cellSize) * cellSize + cellSize;
-      rayPos.y = (player.position.x - rayPos.x) * vtan + player.position.y;
+      vRayPos.x = std::floor(player.position.x / cellSize) * cellSize + cellSize;
+      vRayPos.y = (player.position.x - vRayPos.x) * vtan + player.position.y;
+
       offset.x = cellSize;
       offset.y = -offset.x * vtan;
    }
    else if (std::cos(angle) < -0.001f)
    {
-      rayPos.x = std::floor(player.position.x / cellSize) * cellSize;
-      rayPos.y = (player.position.x - rayPos.x) * vtan + player.position.y;
+      vRayPos.x = std::floor(player.position.x / cellSize) * cellSize - 0.01f;
+      vRayPos.y = (player.position.x - vRayPos.x) * vtan + player.position.y;
+
       offset.x = -cellSize;
       offset.y = -offset.x * vtan;
    }
    else
    {
-      return;
+      vdof = MAX_RAYCAST_DEPTH;
    }
 
-   const auto grid = map.getGrid();
-   for (size_t i = 0; i < MAX_RAYCAST_DEPTH; i++)
+   const auto &grid = map.getGrid();
+   for (; vdof < MAX_RAYCAST_DEPTH; vdof++)
    {
-      int mapX = (int)(rayPos.x / cellSize);
-      int mapY = (int)(rayPos.y / cellSize);
+      int mapX = (int)(vRayPos.x / cellSize);
+      int mapY = (int)(vRayPos.y / cellSize);
 
       if (mapY < grid.size() && mapX < grid[mapY].size() && grid[mapY][mapX])
       {
+         vdist = std::sqrt((vRayPos.x - player.position.x) * (vRayPos.x - player.position.x) +
+                           (vRayPos.y - player.position.y) * (vRayPos.y - player.position.y));
          break;
       }
-      rayPos += offset;
+      vRayPos += offset;
    }
 
-   sf::Vertex line[] = {
-       sf::Vertex(player.position),
-       sf::Vertex(rayPos),
-   };
-   target.draw(line, 2, sf::Lines);
-   return;
-
-   if (sin(angle) > 0.001f)
+   if (std::sin(angle) > 0.001f)
    {
-      rayPos.y = std::floor(player.position.y / cellSize) * cellSize + cellSize;
-      rayPos.x = (player.position.y - rayPos.y) * htan + player.position.x;
+      hRayPos.y = std::floor(player.position.y / cellSize) * cellSize + cellSize;
+      hRayPos.x = (player.position.y - hRayPos.y) * htan + player.position.x;
+
       offset.y = cellSize;
-      offset.x = -offset.x * htan;
+      offset.x = -offset.y * htan;
    }
-   else if (sin(angle) < -0.001f)
+   else if (std::sin(angle) < -0.001f)
    {
-      rayPos.y = std::floor(player.position.y / cellSize) * cellSize;
-      rayPos.x = (player.position.y - rayPos.y) * htan + player.position.x;
+      hRayPos.y = std::floor(player.position.y / cellSize) * cellSize - 0.01f;
+      hRayPos.x = (player.position.y - hRayPos.y) * htan + player.position.x;
+
       offset.y = -cellSize;
       offset.x = -offset.y * htan;
    }
    else
    {
-      return;
+      hdof = MAX_RAYCAST_DEPTH;
    }
+
+   for (; hdof < MAX_RAYCAST_DEPTH; hdof++)
+   {
+      int mapX = (int)(hRayPos.x / cellSize);
+      int mapY = (int)(hRayPos.y / cellSize);
+
+      if (mapY < grid.size() && mapX < grid[mapY].size() && grid[mapY][mapX])
+      {
+         hdist = std::sqrt((hRayPos.x - player.position.x) * (hRayPos.x - player.position.x) +
+                           (hRayPos.y - player.position.y) * (hRayPos.y - player.position.y));
+         break;
+      }
+      hRayPos += offset;
+   }
+
+   sf::Vertex line[] = {
+       sf::Vertex(player.position),
+       sf::Vertex(hdist < vdist ? hRayPos : vRayPos),
+   };
+   target.draw(line, 2, sf::Lines);
 }
